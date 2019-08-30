92cdc74e3185de6732f8667051f02640987fc4ed
sourabh3110 <sourabh@quintype.com>
2019-08-30 16:39:54 +0530

    set chrome path

--- cucumber_framework/project/features/support/browser.rb
+++ cucumber_framework/project/features/support/browser.rb
@@ -4,6 +4,7 @@ require 'byebug'
 class Browser
 
   def open_chrome_browser
+    Selenium::WebDriver::Chrome.driver_path= File.absolute_path("./chromedriver")
     $driver = Selenium::WebDriver.for :chrome
     $driver.manage().window().maximize()
   end
